52617d1ade99aca85ce04a56e527a02eb1321697
vilim <vilim@neuro.mpg.de>
2020-05-22 18:53:41 +0200

    Added more tests in a matrix

--- test/translation.jl
+++ test/translation.jl
@@ -1,19 +1,49 @@
 @testset "Translation-only registration" begin
     imsize = 128
-    values = range(-5f0, stop=5f0, length=imsize)
-    dim_shifts = [-1, 2.9, 3.2]
-    upsampling_paramters = [(upsampling=20, upsample_padding=7), (upsampling=5, upsample_padding=3)]
-    
-    n_dims = 2
-    reference = exp.(-[sum(xs .^2) for xs in Iterators.product([values for _ in 1:n_dims]...)])
-    current_shift = dim_shifts[1:n_dims]
-    moving = warp(reference, Translation(current_shift...), axes(reference), ImageTransformations.Linear(), 0)
+    values = range(-5f0, stop = 5f0, length = imsize)
+    dim_shifts = (-1, 2.9, 3.2)
+    n_images = 5
 
-    tr = find_translation(moving, reference)
-    @test all(tr .== -round.(current_shift))
+    upsampling_paramters = [(upsampling = 10,), (upsampling = 5, upsample_padding = 3)]
 
-    tr = find_translation(moving, reference, upsampling=(10, 10))
-    @show tr current_shift
-    @test all(abs.(tr .+ current_shift) .< 0.3)
+    for (n_dims, ups_params) in zip(2:3, upsampling_paramters)
+        reference =
+            exp.(-[sum(xs .^ 2) for xs in Iterators.product([values for _ in 1:n_dims]...)])
 
+        
+        current_shift =
+            [dim_shifts[1:n_dims] .+ extra_shift for extra_shift in 0:n_images-1]
+        moving = reduce(
+            (x, y) -> cat(x, y, dims = n_dims + 1),
+            reshape(
+                warp(
+                    reference,
+                    Translation(sh...),
+                    axes(reference),
+                    ImageTransformations.Linear(),
+                    0,
+                ), size(reference)...,
+                1,
+            ) for sh in current_shift
+        )
+        
+        im = moving[(Colon() for _ in 1:n_dims)..., 1]
+
+        # test single image
+        tr = find_translation(im, reference)
+        @test all(tr .== .- round.(current_shift[1]))
+
+        tr = find_translation(im, reference; ups_params...)
+        @test all(abs.(tr .+ current_shift[1]) .< 0.3)
+
+        # test image sequence
+        tr = find_translation(moving, reference)
+        @show tr current_shift
+        @test all((all(t .== .- round.(h)) for (h, t) in zip(current_shift, tr)))
+
+        tr = find_translation(moving, reference; ups_params...)
+        @show tr current_shift
+        @test all((all(abs.(h .+ t) .< 0.3) for (h, t) in zip(current_shift, tr)))
+
+    end
 end
